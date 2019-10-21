de1d3bf7784341b8a4d5f011a961a194a232085a
Diógenes <Diogenes1106>
2019-10-21 20:42:40 -0300

    Alterei o Form 2.1.2

--- Validacao.js
+++ Validacao.js
@@ -1,7 +1,39 @@
-function validateForm() {
-  var x = document.forms["cadastro"]["nome"].value;
+/*function verificarNome() {
+  if(document.form.nome.value == ""){
+  alert("Nome não foi preenchido");
+  }
+}
+function verificarNome() {
+  var nome = document.getElementById("nome");
+  if (document.form.nome.value == "") {
+      alert("Nome não foi preenchido");
+  }
+}*/
+function verificarNome(){
+  var x = document.forms["form"]["nome"].value;
   if (x == "") {
-    alert("Name must be filled out");
+    alert("Nome não foi preenchido");
     return false;
   }
 }
+
+function verificarSenha(){
+  if (document.forms.senha1.value == document.forms.senha2.value){
+    alert ("Senhas são iguais")
+  }
+  else{
+    alert ("Senha são diferentes")
+  }
+}
+
+/*function verificarEmail(){
+  if(form.email.value.indexOf("@") == -1 ||
+      form.email.valueOf.indexOf(".") == -1 ||
+      form.email.value == "" ||
+      form.email.value == null) {
+        alert("Por favor, indique um e-mail válido.");
+        //form.email.focus();
+        return false;
+    }
+}*/
+
